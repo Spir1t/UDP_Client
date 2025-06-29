bf5ba0f6dd1ad309f6203bf3ed1237d74fb7ec9e
Spir1t <rinchinov.slava@mail.ru>
2025-06-29 16:08:20 +0300

    Small Changes

--- h/CPU_StatsHandler.h
+++ h/CPU_StatsHandler.h
@@ -1,17 +1,9 @@
 #pragma once
-#include <arpa/inet.h>
 #include <pthread.h>
 #include "CPU_Stats.h"
 
 #define DEST_PORT 1234
 #define DEST_IP "127.0.0.1"
-
-
-typedef struct UdpSenderContext
-{
-    int sockfd;
-    struct sockaddr_in addr;
-} UdpSenderContext;
 
 //Read from /proc/stat
 CPU_Stats * GetStats();
--- src/main.c
+++ src/main.c
@@ -5,45 +5,6 @@
 #include "CPU_Stats.h"
 #include "CPU_StatsHandler.h"
 
-/*
-void print_cpu_stat(const CPU_Stat *stat) {
-    printf("CPU Name     : %s\n", stat->Name);
-    printf("  User       : %u\n", stat->User);
-    printf("  Nice       : %u\n", stat->Nice);
-    printf("  System     : %u\n", stat->System);
-    printf("  Idle       : %u\n", stat->Idle);
-    printf("  Iowait     : %u\n", stat->Iowait);
-    printf("  Irq        : %u\n", stat->Irq);
-    printf("  Softirq    : %u\n", stat->Softirq);
-    printf("  Steal      : %u\n", stat->Steal);
-    printf("  Guest      : %u\n", stat->Guest);
-    printf("  Guest_nice : %u\n", stat->Guest_nice);
-}
-
-void print_cpu_stats(const CPU_Stats *stats) {
-    printf("====== Common CPU Info ======\n");
-    if (&stats->CPU) {
-        print_cpu_stat(&(stats->CPU));
-    }
-
-    printf("\n====== CPU Cores (%u out of %u) ======\n", stats->Cores_Size, stats->Cores_Capacity);
-
-    for (int i = 0; i < stats->Cores_Size; i++) {
-        printf("\n--- Core %d ---\n", i);
-        print_cpu_stat(&stats->CPU_Cores[i]);
-    }
-}
-
-void PrintLoads(float load, float * loads, int size)
-{
-    printf("CPU Load: %f percent\n", load);
-    for(int i = 0; i < size; i++)
-    {
-        printf("CPU%i Load: %f percent\n", i, loads[i]);
-    }
-    free(loads);
-}
-*/
 int main()
 {
     pthread_t sender_thread = start_sender_thread();
